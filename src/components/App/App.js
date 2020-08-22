140a4a79d23a8782be4495f658385146c732d9df
RachelSmith127 <62163543+RachelSmith127@users.noreply.github.com>
2020-08-22 12:34:09 -0500

    initial set up and routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,30 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
+
+      <Route exact path="/feeling" component={Feeling}/>
+      <Route exact path="/understanding" component={Understanding}/>
+      <Route exact path="/support" component={Support}/>
+      <Route exact path="/comments" component={Comments}/>
+      <Route exact path="/review" component={Review}/>
+      <Route exact path="/thankyou" component={Thankyou}/>
         <br/>
       </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (reduxState) => {
+  return{ 
+    reduxState
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { Provider } from 'react-redux';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
